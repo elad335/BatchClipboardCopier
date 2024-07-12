ad516c3f4bf414f237c59a7db6f778d5c6d681fe
Trofimov Yuri <trofimov.yuri.v@gmail.com>
2024-07-12 13:35:38 +0500

    Refactor code Added clipboard image logging and save functionality

---
+++ source/CliboardCopy/Models/ClipboardHistoryItemText.cs
@@ -0,0 +1,11 @@
+﻿namespace CliboardCopy.Models;
+
+public class ClipboardHistoryItemText : ClipboardHistoryItemBase
+{
+    public ClipboardHistoryItemText(string text, DateTime time) : base(time)
+    {
+        Text = text;
+    }
+
+    public string Text { get; }
+}
